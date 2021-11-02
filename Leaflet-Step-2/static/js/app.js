842094391842e998c468c6c2dd849aaded6925cb
Matt DeBone <mdebone@gmail.com>
2021-11-02 11:15:07 -0500

    cleaned leaf2

--- Leaflet-Step-2/static/js/app.js
+++
@@ -1,8 +0,0 @@
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-//
-//
-function createMap(earthquakes) {
-    //
-    function onEachFeature(feature, layer) {
-        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-    }
